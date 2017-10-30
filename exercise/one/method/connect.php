cdcb5860956948281a1fa1df273901fb0121cd16
clingoram <llingchen.27@gmail.com>
2017-10-30 15:38:05 +0800

    Create connect.php

---
+++ exercise/one/method/connect.php
@@ -0,0 +1,15 @@
+<?php  
+
+$dbconnect = 'mysql:host=localhost;port=3306;dbname=course';
+
+try {
+    $connect = new PDO($dbconnect,'nb-lai','123456'); # db,username,pwdof mysql
+    $connect -> query("SET NAMES 'utf8'"); 
+    $connect -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+
+} catch (Exception $e) {
+    echo "Failed to connect with database.Cause: <br> ".$e->getMessage();
+    exit();
+}
+
+?>
