e1b7b5462641f3007bfcf15d9c29c041b2da5f85
clingoram <llingchen.27@gmail.com>
2017-10-30 16:00:01 +0800

    Create course.sql

---
+++ exercise/course.sql
@@ -0,0 +1,17 @@
+create database course;
+use course;
+
+show tables;
+# admin
+select * from thehost;
+
+# user(sign up Or log in)
+select * from theguest;
+
+# post、comment
+select * from posts;
+
+# thehost= admin
+insert into thehost values(null,'Wildboar','etchbones');
+
+# 以上用於admin、guest登入登出，admin帳號密碼已寫死在內，之後會有:新增留言、回應和刪除留言功能。
