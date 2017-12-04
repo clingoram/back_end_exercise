a2574dd80eb3469e13a6167c36128f8789921165
clingoram <llingchen.27@gmail.com>
2017-12-04 09:23:35 +0800

    Update action.php

--- exercise/two/template/method/action.php
+++ exercise/two/template/method/action.php
@@ -8,7 +8,7 @@ header("location:./../user/index.php");
 require 'connect.php';
 
 // insert
-$who = "Wildboar";
+$who = $_SESSION["thehost"]["name"];
 $message = $_POST['message'];
 $save = $_POST['save'];
 $time = date("Y-m-d H:i:s");
@@ -16,13 +16,11 @@ $time = date("Y-m-d H:i:s");
 if(isset($message)&&($save)) {
 
 	if ($_SESSION["thehost"]["name"] == "Wildboar") {
-		$sql = "INSERT INTO posts(idposts,body,post_time,guestreply,guestreply_time,hostreply,hostreply_time,who_say) VALUES(NULL,'$message','$time','',NULL,'',NULL,'$who')";
+		$sql = "INSERT INTO posts(idposts,body,post_time,who_say) VALUES(NULL,'$message','$time','$who')";
 
 		try {
 			$stmt = $connect ->  prepare($sql);
 			$stmt -> execute(array($message,$time,$who));
-			//$arr = $stmt ->errorInfo();
-			//print_r($arr);
 
 		} catch (Exception $e) {
 			echo "Failed to inserted data to MySQL. Cause:<br>".$e->getMessage();
